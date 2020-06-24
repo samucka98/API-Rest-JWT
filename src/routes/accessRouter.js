5be18672007aba0196ef2e9c78a643d39492cde3
samucka98 <samuelibiapinolima@gmail.com>
2020-06-24 10:21:37 -0300

    remoçao de codigo nao utilizado

--- src/routes/accessRouter.js
+++ src/routes/accessRouter.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const accessRouter = express.Router();
 
-const AuthController = require('../controllers/AuthController');
-const authController = new AuthController();
-
 const AccessController = require('../controllers/AccessController');
 const accessController = new AccessController();
 
