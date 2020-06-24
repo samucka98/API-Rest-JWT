feb42dfbbec1ff2473afc5a1b80abdf19e926ec0
samucka98 <samuelibiapinolima@gmail.com>
2020-06-24 10:20:16 -0300

    ajustes nas variaveis de ambiente

--- src/models/migrations/000_createUser.js
+++ src/models/migrations/000_createUser.js
@@ -3,7 +3,8 @@ const knex = require('knex');
 exports.up = async function(knex) {
   return await knex.schema.createTable('User', table => {
     table.increments('id').primary();
-    table.string('avatar');
+    table.string('avatar').notNullable();
+    table.boolean('active').notNullable();
     table.string('firstName').notNullable();
     table.string('lastName').notNullable();
     table.string('email').unique().notNullable();
@@ -13,5 +14,5 @@ exports.up = async function(knex) {
 }
 
 exports.down = async function(knex) {
-  return knex.schema.dropTable('User');
+  return await knex.schema.dropTable('User');
 }
--- src/models/seeds/001_createAdmin.js
+++ src/models/seeds/001_createAdmin.js
@@ -1,15 +1,15 @@
-const knex = require('knex');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '..', '..', '..', '.env') });
 const bcrypt = require('bcryptjs');
-
-const pass = '12345678';
 
 exports.seed = async function(knex) {
   return await knex('User').insert({
-    avatar: 'photo',
-    firstName: 'root',
-    lastName: 'master',
-    email: 'root@master.br',
-    password: bcrypt.hashSync(pass, bcrypt.genSaltSync(12)),
+    avatar: process.env.ADM_AVATAR,
+    active: true,
+    firstName: process.env.ADM_FNAME,
+    lastName: process.env.ADM_LNAME,
+    email: process.env.ADM_MAIL,
+    password: bcrypt.hashSync(process.env.ADM_PASS, bcrypt.genSaltSync(12)),
     date: new Date
   });
 }
